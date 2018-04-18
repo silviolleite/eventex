5c0b6129acd975e87b1cc9db705b728e18b4ddee
Silvio Luis <silvio@orions.com.br>
2018-04-18 14:53:54 -0300

    Habilita inscrição

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET / must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
+
+    def test_subscription_link(self):
+        """Must have /inscricao/ link"""
+        self.assertContains(self.response, 'href="/inscricao/"')
