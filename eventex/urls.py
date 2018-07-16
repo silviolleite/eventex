d446284a798853725d3913e653486e9bca0db8ca
Silvio Leite <silviolleite@gmail.com>
2018-07-16 19:25:25 -0300

    Palestrantes

--- eventex/urls.py
+++ eventex/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-from eventex.core.views import home
+from eventex.core.views import home, speaker_detail
 
 urlpatterns = [
     path('', home, name='home'),
     path('inscricao/', include('eventex.subscriptions.urls')),
+    path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
     path('admin/', admin.site.urls),
 ]
