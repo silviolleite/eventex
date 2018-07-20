00434c8fbc8f1bdee6c12bd2a6ea3a60ef0fb946
Silvio Leite <silviolleite@gmail.com>
2018-07-20 18:18:16 -0300

    Palestras

---
+++ eventex/core/tests/test_model_talk.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from eventex.core.models import Talk
+
+
+class TalkModelTest(TestCase):
+    def setUp(self):
+        self.talk = Talk.objects.create(
+            title='Título da Palestra',
+        )
+
+    def test_create(self):
+        self.assertTrue(Talk.objects.exists())
+
+    def test_has_speakers(self):
+        """Talk has many Speakers and vice-versa"""
+        self.talk.speakers.create(
+            name='Silvio Luis',
+            slug='silvio-luis',
+            website='http://silvioluis.com.br'
+        )
+        self.assertEqual(1, self.talk.speakers.count())
+
+    def test_description_blank(self):
+        field = Talk._meta.get_field('description')
+        self.assertTrue(field.blank)
+
+    def test_speakers_blank(self):
+        field = Talk._meta.get_field('speakers')
+        self.assertTrue(field.blank)
+
+    def test_start_blank(self):
+        field = Talk._meta.get_field('start')
+        self.assertTrue(field.blank)
+
+    def test_start_null(self):
+        field = Talk._meta.get_field('start')
+        self.assertTrue(field.null)
+
+    def test_str(self):
+        self.assertEqual(self.talk.title, str(self.talk))
--- eventex/urls.py
+++ eventex/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-from eventex.core.views import home, speaker_detail
+from eventex.core.views import home, speaker_detail, talk_list
 
 urlpatterns = [
     path('', home, name='home'),
     path('inscricao/', include('eventex.subscriptions.urls')),
     path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
+    path('palestras/', talk_list, name='talk_list'),
     path('admin/', admin.site.urls),
 ]
