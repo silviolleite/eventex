e22c0de6717bf30a6fe5ef0f0254d805ea407118
Silvio Leite <silviolleite@gmail.com>
2018-07-23 19:53:39 -0300

    Cursos como abstract base class

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
-from eventex.core.models import Talk, Speaker
+from eventex.core.models import Talk, Speaker, Course
 
 
 class TestTalkListGet(TestCase):
     def setUp(self):
         t1 = Talk.objects.create(title='Título da Palestra', start='10:00', description='Descrição da palestra.')
         t2 = Talk.objects.create(title='Título da Palestra', start='13:00', description='Descrição da palestra.')
+        c1 = Course.objects.create(title='Título do Curso', start='09:00', description='Descrição do curso.', slots=20)
         speaker = Speaker.objects.create(
             name='Silvio Luis',
             slug='silvio-luis',
@@ -14,6 +15,7 @@ class TestTalkListGet(TestCase):
         )
         t1.speakers.add(speaker)
         t2.speakers.add(speaker)
+        c1.speakers.add(speaker)
         self.resp = self.client.get(r('talk_list'))
 
     def test_get(self):
@@ -27,9 +29,12 @@ class TestTalkListGet(TestCase):
             (2, 'Título da Palestra'),
             (1, '10:00'),
             (1, '13:00'),
-            (2, '/palestrantes/silvio-luis'),
-            (2, 'Silvio Luis'),
+            (3, '/palestrantes/silvio-luis'),
+            (3, 'Silvio Luis'),
             (2, 'Descrição da palestra.'),
+            (1, 'Título do Curso'),
+            (1, '09:00'),
+            (1, 'Descrição do curso'),
         ]
 
         for count, expected in contents:
@@ -37,7 +42,7 @@ class TestTalkListGet(TestCase):
                 self.assertContains(self.resp, expected, count)
 
     def test_context(self):
-        variables = ['morning_talks', 'afternoon_talks']
+        variables = ['morning_talks', 'afternoon_talks', 'courses']
         for key in variables:
             with self.subTest():
                 self.assertIn(key, self.resp.context)
